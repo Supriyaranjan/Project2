a12531ca557eac28470950e9db7b6cb8e1799770
david <david.aldo.dalessandro@gmail.com>
2022-01-12 23:06:56 -0600

    New classes, trigger, and apex completed current apex tests

--- davidPackage/main/default/lwc/orderPage/orderPage.js
+++ davidPackage/main/default/lwc/orderPage/orderPage.js
@@ -1,9 +1,10 @@
+import getAccountId from '@salesforce/apex/orderPageController.findAccount';
 import Id from '@salesforce/user/Id';
 import { LightningElement, api } from 'lwc';
 
 export default class OrderPage extends LightningElement {
     @api order;
-    userId = Id;
+    user = '';
     message = 'Hello from order';
     displayOrder = true;
     showTotal = false;
@@ -14,7 +15,18 @@ export default class OrderPage extends LightningElement {
         for(let i = 0; i < this.order.length; i++){
             this.total += (this.order[i].price * this.order[i].quantity);
         }//end for i
-    }
+    }//end orderTotal
+
+    confirmOrder(){
+        getAccountId({userId: Id})
+        .then(result =>{
+            console.log('Order Confirmed ' + result);
+        })
+        .catch(error =>{
+            console.error(error);
+        })
+        
+    }//end confirmOrder
     
 
 }

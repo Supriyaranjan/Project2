149a50018a7500a8245de027c84eb7900f20f431
Jae Cho <jcho7@pride.hofstra.edu>
2022-01-18 17:17:03 -0500

    final push

--- JohnPackage/main/default/lwc/orderList/orderList.js
+++ JohnPackage/main/default/lwc/orderList/orderList.js
@@ -4,4 +4,14 @@ import getOrderList from '@salesforce/apex/OrderController.getOrderList';
 export default class OrderList extends LightningElement {
    @wire(getOrderList) orders; 
 
+   handleSelect(event) {
+      
+      event.preventDefault();
+      
+      const selectEvent = new CustomEvent('orderselect', {
+          detail: { orderId: event.currentTarget.dataset.orderId }
+      });
+      
+      this.dispatchEvent(selectEvent);
+  }
 }
